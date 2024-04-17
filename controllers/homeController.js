ef5d12990202048c8cbb24604b42f5386f92056c
leesanghoit <160556543+leesanghoit@users.noreply.github.com>
2024-04-17 02:27:52 +0000

    집에서 할 것.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,7 +5,7 @@
  * Listing 12.5 (p. 178)
  * 홈 컨트롤러로의 라우팅
  */
-
+exports.respondWithName1; // @TODO: 사용자 정의 EJS 뷰를 사용한 응답
 
 /**
  * Listing 12.9 (p. 182)
@@ -14,3 +14,19 @@
  * [노트] 뷰 안에서는 offeredCourses라는 변수명을 통해 배열에 접근할 수 있다.
  * 홈 컨트롤러 내에서 이 배열은 courses라는 이름으로 다룬다.
  */
+exports.respondWithName = (req, res) => {
+    let paramsName = req.params.myName;
+    res.render('index', {
+        name: paramsName,
+        people: {
+            first: {
+                name: "Mary",
+                city: "Jeonju"
+            },
+            second: {
+                name: "Oppenheimer",
+                city: "Los Alamos"
+            }
+        }
+    });    
+}; // @TODO: 요청 매개변수로 지역 변수 할당. 그다음 렌더링된 뷰로 지역 변수 전달.
--- main.js
+++ main.js
@@ -3,12 +3,26 @@
 "use strict";
 
 // 앱 설정
+const port = 3000,
+    express = require('express'),
+    layouts = require('express-ejs-layouts'),
+    homeController = require('./controllers/homeController'),
+    errorController = require('./controllers/errorController'),
+    app = express();
 
 /**
  * Listing 12.7 (p. 179)
  * ejs 레이아웃 렌더링
  */
+app.set("port", process.env.PORT || port)
+app.set("view engine", "ejs");
 
+app.use(layouts); // layout.ejs 를 쓸 수 있다.
+app.use(express.static("public")); // 정적 파일 디렉토리
+
+app.get('/name/:myName', homeController.respondWithName);
+// app.get(); = GET method
+// app.post(); = POST method
 
 /**
  * Listing 12.4 (p. 177)
@@ -20,12 +34,17 @@
  * Listing 12.6 (p. 178)
  * 각 페이지 및 요청 타입을 위한 라우트 추가
  */
-
+app.get('/name/:myName', homeController.respondWithName);
 
 /**
  * Listing 12.12 (p. 184)
  * 에러 처리 라우트 
  */
-
+app.use(errorController.logErrors);
+app.use(errorController.resNotFound);
+app.use(errorController.resInternalError);
 
 // 3000번 포트로 리스닝 설정
+app.listen(app.get("port"), () => {
+    console.log(`Server at http://localhost:${app.get("port")}`);
+});
